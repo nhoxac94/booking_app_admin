39e512db56147ed0ed8ef05211706f18424d6d05
Le Hoang Loi <nhoxac94@yahoo.com>
2022-08-24 13:19:28 +0700

    first comment

---
+++ src/datatablesource.js
@@ -0,0 +1,91 @@
+export const userColumns = [
+  { field: "_id", headerName: "ID", width: 250 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img
+            className="cellImg"
+            src={
+              params.row.image ||
+              "https://www.pinpng.com/pngs/m/341-3416159_no-avatar-pic-user-profile-image-png-transparent.png"
+            }
+            alt="avatar"
+          />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 230,
+  },
+  {
+    field: "country",
+    headerName: "Country",
+    width: 100,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 100,
+  },
+];
+
+export const hotelColumns = [
+  { field: "_id", headerName: "ID", width: 250 },
+  {
+    field: "name",
+    headerName: "Name",
+    width: 230,
+  },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 200,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 300,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+];
+
+export const roomColumns = [
+  { field: "_id", headerName: "ID", width: 250 },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 100,
+  },
+  {
+    field: "desc",
+    headerName: "Description",
+    width: 250,
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 150,
+  },
+  {
+    field: "maxPeople",
+    headerName: "Max People",
+    width: 100,
+  },
+];
---
+++ src/formSource.js
@@ -0,0 +1,114 @@
+export const userInputs = [
+  {
+    id: "name",
+    label: "Name",
+    type: "text",
+    placeholder: "My Hotel",
+  },
+  {
+    id: "type",
+    label: "Type",
+    type: "type",
+    placeholder: "hotel",
+  },
+  {
+    id: "phone",
+    label: "Phone",
+    type: "text",
+    placeholder: "+084 123456",
+  },
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: "country",
+    label: "Country",
+    type: "country",
+    placeholder: "VN",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "city",
+    placeholder: "HCM",
+  },
+];
+
+export const hotelInputs = [
+  {
+    id: "username",
+    label: "Username",
+    type: "text",
+    placeholder: "john_doe",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "john_doe@gmail.com",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "HCM",
+  },
+  {
+    id: "address",
+    label: "Address",
+    type: "address",
+  },
+  {
+    id: "distance",
+    label: "Distance",
+    type: "distance",
+    placeholder: "500",
+  },
+  {
+    id: "title",
+    label: "Title",
+    type: "title",
+    placeholder: "The best hotel",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "description",
+    placeholder: "Description",
+  },
+  {
+    id: "price",
+    label: "Price",
+    type: "price",
+    placeholder: "100",
+  },
+];
+
+export const roomInputs = [
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "2 bed room",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "desc",
+    placeholder: "King size bed, 1 bathroom",
+  },
+  {
+    id: "price",
+    label: "Price",
+    type: "number",
+    placeholder: "100",
+  },
+  {
+    id: "maxPeople",
+    label: "Max People",
+    type: "number",
+    placeholder: "2",
+  },
+];
